8c78664c5412ea7db85bf71607d466c64ac961a1
alfer91 <dasungwkd@naver.com>
2021-08-16 04:13:26 +0900

    Page Top Move (ScrollTo)

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ searchInputEl.addEventListener('blur', function () {
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
@@ -27,16 +28,30 @@ window.addEventListener('scroll', _.throttle(function () {
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
-    })
+    });
+    // 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
   } else {
     // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
-    })
+    });
+    // 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -60,9 +75,9 @@ new Swiper('.promotion .swiper-container', {
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
-  // autoplay: {
-  //   delay: 5000
-  // },
+  autoplay: {
+    delay: 5000
+  },
   pagination: {
     el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
     clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
